077559e386de3285a777dd69f2ab5c9163a30c2e
Abhishek Sah <sah755146@gmail.com>
2022-02-02 23:38:58 +0530

    text recognition works

--- android/app/src/main/java/com/aush/aush/mlkit/MLKitTextRecognitionModule.java
+++ android/app/src/main/java/com/aush/aush/mlkit/MLKitTextRecognitionModule.java
@@ -1,6 +1,9 @@
 package com.aush.aush.mlkit;
 
 import android.annotation.SuppressLint;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.net.Uri;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -8,6 +11,16 @@ import androidx.annotation.NonNull;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
+
+import java.io.IOException;
 
 public class MLKitTextRecognitionModule extends ReactContextBaseJavaModule {
     MLKitTextRecognitionModule(ReactApplicationContext context) {
@@ -24,5 +37,51 @@ public class MLKitTextRecognitionModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void extractWords(String url) {
         Log.d("MLKitTextRecognitionModule", "url= "+url);
+
+
+        InputImage image;
+        Uri uri = Uri.parse(url);
+        try {
+            image = InputImage.fromFilePath(getReactApplicationContext(),uri);
+            TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+
+            Task<Text> result =
+                    recognizer.process(image)
+                            .addOnSuccessListener(new OnSuccessListener<Text>() {
+                                @Override
+                                public void onSuccess(Text result) {
+                                    // Task completed successfully
+                                    String resultText = result.getText();
+                                    for (Text.TextBlock block : result.getTextBlocks()) {
+                                        String blockText = block.getText();
+                                        Point[] blockCornerPoints = block.getCornerPoints();
+                                        Rect blockFrame = block.getBoundingBox();
+                                        for (Text.Line line : block.getLines()) {
+                                            String lineText = line.getText();
+                                            Point[] lineCornerPoints = line.getCornerPoints();
+                                            Rect lineFrame = line.getBoundingBox();
+                                            for (Text.Element element : line.getElements()) {
+                                                String elementText = element.getText();
+                                                Point[] elementCornerPoints = element.getCornerPoints();
+                                                Rect elementFrame = element.getBoundingBox();
+                                            }
+                                        }
+                                    }
+                                }
+                            })
+                            .addOnFailureListener(
+                                    new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            // Task failed with an exception
+                                            // ...
+                                        }
+                                    });
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
