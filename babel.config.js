0bcafb6f666131436d00cc85d221aea6a77af7e0
Abhishek Sah <sah755146@gmail.com>
2022-02-05 04:00:39 +0530

    refactored most

--- babel.config.js
+++ babel.config.js
@@ -2,20 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["module:metro-react-native-babel-preset"],
-    plugins: [
-      ["@babel/plugin-proposal-decorators", { legacy: true }],
-      // ["@babel/plugin-proposal-class-properties", { loose: true }],
-      [
-        "module:react-native-dotenv",
-        {
-          moduleName: "@env",
-          path: ".env",
-          blacklist: null,
-          whitelist: null,
-          safe: true,
-          allowUndefined: false,
-        },
-      ],
-    ],
+    plugins: [["@babel/plugin-proposal-decorators", { legacy: true }]],
   };
 };
