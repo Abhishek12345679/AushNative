f646aafa441a0fea3dd5433191a0ea4bd51fce5b
Abhishek Sah <sah755146@gmail.com>
2022-02-06 16:24:49 +0530

    minor changes and trying out react-native-vision-camera

--- screens/AddAddressModalScreen.js
+++ screens/AddAddressModalScreen.js
@@ -1,145 +1,134 @@
-import React, { useState } from "react";
+import React, {useState} from 'react';
 import {
   KeyboardAvoidingView,
   ScrollView,
   TextInput,
-  TouchableOpacity,
   StatusBar,
   Button,
-} from "react-native";
+  ActivityIndicator,
+} from 'react-native';
 
-import { Formik } from "formik";
-import { Ionicons } from "@expo/vector-icons";
-import RadioButtonRN from "radio-buttons-react-native";
+import {Formik} from 'formik';
+import RadioButtonRN from 'radio-buttons-react-native';
 
-import { showMessage } from "react-native-flash-message";
-import { observer } from "mobx-react";
-import addAddresses from "../helpers/addAddress";
-import { Pressable } from "react-native";
+import {showMessage} from 'react-native-flash-message';
+import {observer} from 'mobx-react';
+import addAddresses from '../helpers/addAddress';
+import {Pressable} from 'react-native';
 
-const AddAddressModalScreen = observer((props) => {
+const AddAddressModalScreen = observer(props => {
   const [addingAddress, setAddingAddress] = useState(false);
-  const [type, setType] = useState("Home");
+  const [type, setType] = useState('Home');
 
-  const data = [
-    {
-      label: "Home",
-    },
-    {
-      label: "Work",
-    },
-    {
-      label: "Other",
-    },
-  ];
+  const AddressType = ['Home', 'Work', 'Other'];
 
   return (
-    <ScrollView style={{ padding: 20 }}>
+    <ScrollView style={{padding: 20}}>
       <StatusBar barStyle="light-content" />
       <KeyboardAvoidingView behavior="position">
         <Formik
           initialValues={{
-            type: "",
-            name: "",
-            add_line_1: "",
-            add_line_2: "",
-            pincode: "",
-            ph_no: "",
+            type: '',
+            name: '',
+            add_line_1: '',
+            add_line_2: '',
+            pincode: '',
+            ph_no: '',
           }}
-          onSubmit={async (values) => {
-            console.log("Submitting...");
+          onSubmit={async values => {
+            console.log('Submitting...');
 
+            addingAddress(true);
             await addAddresses({
               type: type,
               name: values.name,
               add_line_1: values.add_line_1,
               add_line_2: values.add_line_2,
               pincode: values.pincode,
-              ph_no: "+91" + values.ph_no,
+              ph_no: '+91' + values.ph_no,
             });
 
             showMessage({
-              message: "New Address Added",
-              type: "success",
+              message: 'New Address Added',
+              type: 'success',
               duration: 1000,
             });
 
+            addingAddress(true);
             props.navigation.pop();
-          }}
-        >
-          {({ handleChange, handleSubmit, values }) => (
+          }}>
+          {({handleChange, handleSubmit, values}) => (
             <ScrollView>
               <RadioButtonRN
                 initial={1}
-                data={data}
-                selectedBtn={(e) => {
-                  console.log(e);
-                  setType(e.label);
+                data={AddressType}
+                selectedBtn={item => {
+                  setType(item);
                 }}
                 circleSize={16}
               />
               <TextInput
-                onChangeText={handleChange("name")}
+                onChangeText={handleChange('name')}
                 value={values.name}
                 style={{
                   height: 60,
                   marginTop: 20,
                   borderBottomWidth: 1,
-                  borderColor: "#ccc",
+                  borderColor: '#ccc',
                 }}
-                placeholder={"Name"}
+                placeholder={'Name'}
                 placeholderTextColor="#a7a8a9"
               />
 
               <TextInput
                 keyboardType="phone-pad"
-                onChangeText={handleChange("ph_no")}
+                onChangeText={handleChange('ph_no')}
                 value={values.ph_no}
                 style={{
                   height: 60,
                   marginTop: 20,
                   borderBottomWidth: 1,
-                  borderColor: "#ccc",
+                  borderColor: '#ccc',
                 }}
-                placeholder={"Mobile"}
+                placeholder={'Mobile'}
                 placeholderTextColor="#a7a8a9"
               />
               <TextInput
                 maxLength={6}
                 keyboardType="number-pad"
-                onChangeText={handleChange("pincode")}
+                onChangeText={handleChange('pincode')}
                 value={values.pincode}
                 style={{
                   height: 60,
                   marginTop: 20,
                   borderBottomWidth: 1,
-                  borderColor: "#ccc",
+                  borderColor: '#ccc',
                 }}
-                placeholder={"pincode"}
+                placeholder={'pincode'}
                 placeholderTextColor="#a7a8a9"
               />
               <TextInput
-                onChangeText={handleChange("add_line_1")}
+                onChangeText={handleChange('add_line_1')}
                 value={values.add_line_1}
                 style={{
                   height: 60,
                   marginTop: 20,
                   borderBottomWidth: 1,
-                  borderColor: "#ccc",
+                  borderColor: '#ccc',
                 }}
-                placeholder={"Address Line 1"}
+                placeholder={'Address Line 1'}
                 placeholderTextColor="#a7a8a9"
               />
               <TextInput
-                onChangeText={handleChange("add_line_2")}
+                onChangeText={handleChange('add_line_2')}
                 value={values.add_line_2}
                 style={{
                   height: 60,
                   marginTop: 20,
                   borderBottomWidth: 1,
-                  borderColor: "#ccc",
+                  borderColor: '#ccc',
                 }}
-                placeholder={"Address Line 2"}
+                placeholder={'Address Line 2'}
                 placeholderTextColor="#a7a8a9"
               />
               <Button
@@ -154,15 +143,18 @@ const AddAddressModalScreen = observer((props) => {
                 style={{
                   width: 200,
                   height: 70,
-                  backgroundColor: "#000",
+                  backgroundColor: '#000',
                 }}
                 android_ripple={{
-                  color: "#fff",
+                  color: '#fff',
                   borderless: true,
                 }}
-                onPress={handleSubmit}
-              >
-                <Text>Add Address</Text>
+                onPress={handleSubmit}>
+                {!addingAddress ? (
+                  <Text>Add Address</Text>
+                ) : (
+                  <ActivityIndicator size="small" />
+                )}
               </Pressable>
             </ScrollView>
           )}
