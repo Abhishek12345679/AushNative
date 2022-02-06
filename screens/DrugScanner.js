c23714c5ac9a3069be83e430b550b352f7bc06c4
Abhishek Sah <sah755146@gmail.com>
2022-02-06 14:44:35 +0530

    removed deprecated expo-permissions

--- screens/DrugScanner.js
+++ screens/DrugScanner.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, {useState, useEffect, useRef} from 'react';
 import {
   Text,
   View,
@@ -7,28 +7,25 @@ import {
   KeyboardAvoidingView,
   Platform,
   Dimensions,
-} from "react-native";
+} from 'react-native';
 
-import GestureRecognizer from "react-native-swipe-gestures";
-import { useFocusEffect } from "@react-navigation/native";
+import GestureRecognizer from 'react-native-swipe-gestures';
+import {useFocusEffect} from '@react-navigation/native';
 
-import * as Permissions from "expo-permissions";
-import * as ImagePicker from "expo-image-picker";
-import Modal from "react-native-modal";
+import * as ImagePicker from 'expo-image-picker';
+import {Camera} from 'expo-camera';
+import Modal from 'react-native-modal';
 
-import { Camera } from "expo-camera";
+import {MaterialIcons, Ionicons, Entypo} from '@expo/vector-icons';
+import RoundButton from '../components/RoundButton';
+import ManualSearchBox from '../components/ManualSearchBox';
+import {colors} from '../constants/colors';
 
-import { MaterialIcons, Ionicons, Entypo } from "@expo/vector-icons";
-import RoundButton from "../components/RoundButton";
-import ManualSearchBox from "../components/ManualSearchBox";
-import { colors } from "../constants/colors";
-
-const DrugScanner = (props) => {
+const DrugScanner = props => {
   const [cameraRef, setCameraRef] = useState(null);
   const [camera, setCamera] = useState(null);
   const [mounted, setMounted] = useState(true);
 
-  const baseUri = "data:image/jpg;base64,";
   const contourRef = useRef(null);
 
   const [hasPermission, setHasPermission] = useState(null);
@@ -37,17 +34,17 @@ const DrugScanner = (props) => {
   const [isCameraReady, setIsCameraReady] = useState(false);
 
   const [isVisible, setIsVisible] = useState(false);
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState('');
   const [selectedIndex, setSelectedIndex] = useState(0);
 
   const [flashStatus, setFlashStatus] = useState(
-    Camera.Constants.FlashMode.off
+    Camera.Constants.FlashMode.off,
   );
 
   // Screen Ratio and image padding
   const [imagePadding, setImagePadding] = useState(0);
-  const [ratio, setRatio] = useState("4:3"); // default is 4:3
-  const { height, width } = Dimensions.get("window");
+  const [ratio, setRatio] = useState('4:3');
+  const {height, width} = Dimensions.get('window');
   const screenRatio = height / width;
   const [isRatioSet, setIsRatioSet] = useState(false);
 
@@ -63,7 +60,7 @@ const DrugScanner = (props) => {
       if (!photo.cancelled) {
         // props.navigation.pop();
 
-        props.navigation.navigate("Confirm", {
+        props.navigation.navigate('Confirm', {
           photo: photo,
         });
       }
@@ -77,11 +74,10 @@ const DrugScanner = (props) => {
   useEffect(() => {
     (async () => {
       try {
-        const { CameraStatus } = await Camera.requestPermissionsAsync();
-        const { GalleryStatus } = await Permissions.askAsync(
-          Permissions.CAMERA_ROLL
-        );
-        setHasPermission(CameraStatus && GalleryStatus === "granted");
+        const {CameraStatus} = await Camera.requestCameraPermissionsAsync();
+        const {GalleryStatus} =
+          await ImagePicker.requestMediaLibraryPermissionsAsync();
+        setHasPermission(CameraStatus && GalleryStatus === 'granted');
       } catch (e) {
         console.error(e);
       }
@@ -103,17 +99,17 @@ const DrugScanner = (props) => {
     React.useCallback(() => {
       setMounted(true);
       return () => {
-        console.log("unmounted");
+        console.log('unmounted');
         setMounted(false);
       };
-    }, [])
+    }, []),
   );
 
   //fixing ratio
   const prepareRatio = async () => {
-    let desiredRatio = "4:3"; // Start with the system default
+    let desiredRatio = '4:3'; // Start with the system default
     // This issue only affects Android
-    if (Platform.OS === "android") {
+    if (Platform.OS === 'android') {
       const ratios = await camera.getSupportedRatiosAsync();
       // console.log(ratios);
 
@@ -124,7 +120,7 @@ const DrugScanner = (props) => {
       let realRatios = {};
       let minDistance = null;
       for (const ratio of ratios) {
-        const parts = ratio.split(":");
+        const parts = ratio.split(':');
         const realRatio = parseInt(parts[0]) / parseInt(parts[1]);
         realRatios[ratio] = realRatio;
         // ratio can't be taller than screen, so we don't want an abs()
@@ -142,7 +138,7 @@ const DrugScanner = (props) => {
       desiredRatio = minDistance;
       //  calculate the difference between the camera width and the screen height
       const remainder = Math.floor(
-        (height - realRatios[desiredRatio] * width) / 2
+        (height - realRatios[desiredRatio] * width) / 2,
       );
       // set the preview padding and preview ratio
       setImagePadding(remainder / 2);
@@ -153,7 +149,7 @@ const DrugScanner = (props) => {
     }
   };
 
-  const searchQueryChangeHandler = (text) => {
+  const searchQueryChangeHandler = text => {
     setQuery(text);
   };
 
@@ -178,7 +174,7 @@ const DrugScanner = (props) => {
       setType(
         type === Camera.Constants.Type.back
           ? Camera.Constants.Type.front
-          : Camera.Constants.Type.back
+          : Camera.Constants.Type.back,
       );
     }
     lastPress = time;
@@ -209,7 +205,7 @@ const DrugScanner = (props) => {
           // quality: 0.5,
         });
 
-        props.navigation.navigate("Confirm", {
+        props.navigation.navigate('Confirm', {
           // mode:'camera',
           photo: photo,
         });
@@ -218,66 +214,60 @@ const DrugScanner = (props) => {
   };
 
   return (
-    <View style={{ flex: 1 }}>
+    <View style={{flex: 1}}>
       <GestureRecognizer
-        onSwipeUp={(state) => onSwipeUp(state)}
-        onSwipeDown={(state) => onSwipeDown(state)}
+        onSwipeUp={state => onSwipeUp(state)}
+        onSwipeDown={state => onSwipeDown(state)}
         config={config}
         style={{
           flex: 1,
-          backgroundColor: "#14213d",
-        }}
-      >
+          backgroundColor: '#14213d',
+        }}>
         {mounted && (
           <Camera
             ratio={ratio}
             style={{
               flex: 1,
-              backgroundColor: "#000",
-              flexDirection: "row",
+              backgroundColor: '#000',
+              flexDirection: 'row',
               marginTop: imagePadding,
               marginBottom: imagePadding,
             }}
             type={type}
             flashMode={flashStatus}
-            ref={(ref) => {
+            ref={ref => {
               setCamera(ref);
               setCameraRef(ref);
             }}
             onCameraReady={onCameraReady}
-            onStartShouldSetResponder={(evt) => onDoublePress()}
-            useCamera2Api
-          >
+            onStartShouldSetResponder={evt => onDoublePress()}
+            useCamera2Api>
             <View
               style={{
                 flex: 0.8,
-                backgroundColor: "#ffffff00",
-                flexDirection: "column",
-                justifyContent: "space-around",
-                alignItems: "center",
+                backgroundColor: '#ffffff00',
+                flexDirection: 'column',
+                justifyContent: 'space-around',
+                alignItems: 'center',
                 marginStart: 40,
                 marginBottom: -30,
                 marginVertical: 150,
-              }}
-            >
+              }}>
               <View
                 ref={contourRef.current}
                 style={{
-                  width: "95%",
+                  width: '95%',
                   height: 100,
-                  justifyContent: "center",
-                  alignItems: "center",
+                  justifyContent: 'center',
+                  alignItems: 'center',
                   marginTop: 15,
                 }}
               />
               <TouchableOpacity
                 activeOpacity={0.75}
                 style={styles.captureButton}
-                onPress={captureImage}
-              >
-                <Text
-                  style={{ color: "#fff", fontSize: 20, fontWeight: "bold" }}
-                >
+                onPress={captureImage}>
+                <Text style={{color: '#fff', fontSize: 20, fontWeight: 'bold'}}>
                   Capture
                 </Text>
               </TouchableOpacity>
@@ -285,49 +275,44 @@ const DrugScanner = (props) => {
             <View
               style={{
                 flex: 0.2,
-                backgroundColor: "#ffffff00",
-                flexDirection: "column",
-                justifyContent: "flex-start",
-                alignItems: "flex-end",
-              }}
-            >
+                backgroundColor: '#ffffff00',
+                flexDirection: 'column',
+                justifyContent: 'flex-start',
+                alignItems: 'flex-end',
+              }}>
               <TouchableOpacity
                 style={{
-                  alignSelf: "flex-end",
-                  alignItems: "flex-end",
+                  alignSelf: 'flex-end',
+                  alignItems: 'flex-end',
                   marginTop: 30,
                   marginEnd: 10,
-                }}
-              >
+                }}>
                 <RoundButton
-                  style={{ backgroundColor: "rgba(0,0,0,0.4)" }}
+                  style={{backgroundColor: 'rgba(0,0,0,0.4)'}}
                   onPress={() => {
                     props.navigation.goBack();
-                  }}
-                >
+                  }}>
                   <Entypo name="cross" size={25} color="#fff" />
                 </RoundButton>
               </TouchableOpacity>
               <TouchableOpacity
                 style={{
-                  alignSelf: "flex-end",
-                  alignItems: "flex-end",
+                  alignSelf: 'flex-end',
+                  alignItems: 'flex-end',
                   marginTop: 10,
                   marginEnd: 10,
-                }}
-              >
+                }}>
                 <RoundButton
-                  style={{ backgroundColor: "rgba(0,0,0,0.4)" }}
+                  style={{backgroundColor: 'rgba(0,0,0,0.4)'}}
                   onPress={() => {
                     setType(
                       type === Camera.Constants.Type.front
                         ? Camera.Constants.Type.back
-                        : Camera.Constants.Type.front
+                        : Camera.Constants.Type.front,
                     );
-                  }}
-                >
+                  }}>
                   <MaterialIcons
-                    name={type === 2 ? "camera-front" : "camera-rear"}
+                    name={type === 2 ? 'camera-front' : 'camera-rear'}
                     size={20}
                     color="#fff"
                   />
@@ -335,20 +320,18 @@ const DrugScanner = (props) => {
               </TouchableOpacity>
               <TouchableOpacity
                 style={{
-                  alignSelf: "flex-end",
-                  alignItems: "flex-end",
+                  alignSelf: 'flex-end',
+                  alignItems: 'flex-end',
                   marginEnd: 10,
                   marginTop: 10,
-                }}
-              >
+                }}>
                 <RoundButton
-                  style={{ backgroundColor: "rgba(0,0,0,0.4)" }}
+                  style={{backgroundColor: 'rgba(0,0,0,0.4)'}}
                   onPress={() => {
                     setFlashStatus(flashStatus === 0 ? 1 : 0);
-                  }}
-                >
+                  }}>
                   <Ionicons
-                    name={flashStatus === 0 ? "ios-flash-off" : "ios-flash"}
+                    name={flashStatus === 0 ? 'ios-flash-off' : 'ios-flash'}
                     size={20}
                     color="#fff"
                   />
@@ -356,33 +339,29 @@ const DrugScanner = (props) => {
               </TouchableOpacity>
               <TouchableOpacity
                 style={{
-                  alignSelf: "flex-end",
-                  alignItems: "flex-end",
+                  alignSelf: 'flex-end',
+                  alignItems: 'flex-end',
                   marginEnd: 10,
                   marginTop: 10,
-                }}
-              >
+                }}>
                 <RoundButton
-                  style={{ backgroundColor: "rgba(0,0,0,0.4)" }}
+                  style={{backgroundColor: 'rgba(0,0,0,0.4)'}}
                   onPress={() => {
-                    setIsVisible((prev) => !prev);
-                  }}
-                >
+                    setIsVisible(prev => !prev);
+                  }}>
                   <Ionicons name="ios-search" size={20} color="#fff" />
                 </RoundButton>
               </TouchableOpacity>
               <TouchableOpacity
                 style={{
-                  alignSelf: "flex-end",
-                  alignItems: "flex-end",
+                  alignSelf: 'flex-end',
+                  alignItems: 'flex-end',
                   marginEnd: 10,
                   marginTop: 10,
-                }}
-              >
+                }}>
                 <RoundButton
                   onPress={pickImage}
-                  style={{ backgroundColor: "rgba(0,0,0,0.4)" }}
-                >
+                  style={{backgroundColor: 'rgba(0,0,0,0.4)'}}>
                   <MaterialIcons name="photo-library" size={20} color="#fff" />
                 </RoundButton>
               </TouchableOpacity>
@@ -390,35 +369,32 @@ const DrugScanner = (props) => {
             <KeyboardAvoidingView>
               <View
                 style={{
-                  width: "100%",
-                  justifyContent: "center",
-                  alignItems: "center",
+                  width: '100%',
+                  justifyContent: 'center',
+                  alignItems: 'center',
                   marginBottom: 50,
-                }}
-              ></View>
+                }}></View>
             </KeyboardAvoidingView>
             <Modal
               avoidKeyboard={true}
               isVisible={isVisible}
               onSwipeComplete={() => setIsVisible(false)}
-              swipeDirection={["down"]}
+              swipeDirection={['down']}
               style={{
-                justifyContent: "flex-end",
+                justifyContent: 'flex-end',
                 margin: 0,
               }}
-              onBackdropPress={() => setIsVisible(false)}
-            >
+              onBackdropPress={() => setIsVisible(false)}>
               <View
                 style={{
-                  width: "100%",
+                  width: '100%',
                   height: 20,
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}>
                 <View
                   style={{
-                    backgroundColor: "#ccc",
+                    backgroundColor: '#ccc',
                     width: 50,
                     height: 8,
                     borderRadius: 5,
@@ -427,25 +403,24 @@ const DrugScanner = (props) => {
               </View>
               <View
                 style={{
-                  height: "75%",
-                  backgroundColor: "#FFF",
+                  height: '75%',
+                  backgroundColor: '#FFF',
                   padding: 22,
                   borderTopEndRadius: 15,
                   borderTopStartRadius: 15,
-                }}
-              >
+                }}>
                 <ManualSearchBox
                   onpress={() => {
                     setIsVisible(false);
-                    props.navigation.navigate("Results", {
+                    props.navigation.navigate('Results', {
                       query: query,
                       manual_search: true,
-                      mode: selectedIndex === 0 ? "name" : "salt",
+                      mode: selectedIndex === 0 ? 'name' : 'salt',
                     });
                   }}
                   onchangeText={searchQueryChangeHandler}
                   value={query}
-                  onchange={(event) => {
+                  onchange={event => {
                     setSelectedIndex(event.nativeEvent.selectedSegmentIndex);
                   }}
                   selectedIndex={selectedIndex}
@@ -469,8 +444,8 @@ const styles = StyleSheet.create({
     backgroundColor: colors.PRIMARY,
     borderRadius: 50,
     elevation: 20, //android only
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
