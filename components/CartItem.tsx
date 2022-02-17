32cc87fa4fdd50b924047ff5cb4aae26ee5c2538
Abhishek12345679 <sah755146@gmail.com>
2022-02-17 23:16:38 +0530

    darkened cart item

--- components/CartItem.tsx
+++ components/CartItem.tsx
@@ -1,7 +1,8 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, StyleSheet } from 'react-native'
 import React from 'react'
 import { DrugType } from '../store/CartStore'
-import { FontAwesome } from '@expo/vector-icons'
+import { FontAwesome, FontAwesome5 } from '@expo/vector-icons'
+import { colors } from '../constants/colors'
 
 interface CartItemProps {
     keyProp: number | string;
@@ -15,63 +16,90 @@ const CartItem = ({ keyProp, drug, removeFromCart }: CartItemProps) => {
         <View
             key={keyProp}
             style={{
+                width: '100%',
                 flexDirection: 'column',
-                backgroundColor: '#fff',
+                backgroundColor: colors.SECONDARY,
+                borderRadius: 10,
+                height: 130
             }}>
             <View
                 style={{
                     flexDirection: 'row',
                     alignItems: 'center',
+                    justifyContent: "space-evenly",
                     marginEnd: 10,
                     marginTop: 10,
+                    marginStart: 20,
                     borderRadius: 20,
                 }}>
                 <Image
                     source={{ uri: drug.imageUrl }}
                     style={{
-                        width: 75,
-                        height: 75,
-                        marginEnd: 10,
+                        width: 50,
+                        height: 50,
+                        marginEnd: 20,
+                        borderRadius: 5,
                     }}
                 />
                 <View style={{ marginEnd: 10 }}>
-                    <Text style={{ fontSize: 20, fontWeight: 'bold' }}>
+                    <Text style={{ fontSize: 20, fontWeight: 'bold', color: "#fff" }}>
                         {drug.name}
                     </Text>
-                    <Text style={{ fontSize: 15, width: 200 }}>{drug.salt}</Text>
+                    <Text style={{ fontSize: 15, width: 200, color: "#fff" }}>{drug.salt}</Text>
                 </View>
+                <FontAwesome
+                    name="trash"
+                    size={24}
+                    color="#fff"
+                    onPress={removeFromCart}
+                />
             </View>
-
-            <View style={{ marginStart: 10, marginVertical: 10 }}>
-                {/* <QuantitySelector /> */}
+            <View
+                style={{
+                    flexDirection: 'row',
+                    marginHorizontal: 25,
+                    marginTop: 20
+                }}
+            >
+                <View style={styles.item}>
+                    <FontAwesome name="balance-scale" color="#fff" size={15} />
+                    <Text style={styles.subtext}>{drug.quantity}</Text>
+                </View>
+                <View style={styles.item}>
+                    <FontAwesome5 name="money-bill-wave" color="#fff" size={15} />
+                    <Text style={styles.subtext}>₹{drug.price}</Text>
+                </View>
                 <View
                     style={{
-                        flexDirection: 'row',
+                        flexDirection: 'column',
+                        marginTop: -15,
                         alignItems: 'center',
-                        justifyContent: 'space-between',
-                        paddingHorizontal: 10,
                     }}>
-                    <View
-                        style={{
-                            flexDirection: 'row',
-                            alignItems: 'center',
-                            justifyContent: 'space-around',
-                        }}>
-                        <Text style={{ color: 'green' }}>₹ {drug.price}</Text>
-                        <Text> x {drug.quantity} = </Text>
-                        <Text style={{ color: 'green', fontWeight: 'bold' }}>
-                            ₹{drug.total_amt.toFixed(2)}
-                        </Text>
+                    <Text style={{ color: "#fff", fontWeight: 'bold', marginBottom: -5 }}>Total</Text>
+                    <View style={styles.item}>
+                        <FontAwesome5 name="money-bill-wave" color="#fff" size={15} />
+                        <Text style={styles.subtext}>₹{drug.total_amt}</Text>
                     </View>
-                    <FontAwesome
-                        name="trash"
-                        size={24}
-                        onPress={removeFromCart}
-                    />
                 </View>
             </View>
-        </View>
+        </View >
     )
 }
+
+const styles = StyleSheet.create({
+    subtext: {
+        color: "#fff",
+        fontWeight: "bold",
+        fontSize: 17,
+        marginStart: 10
+    },
+    item: {
+        height: 40,
+        width: 110,
+        flexDirection: 'row',
+        alignItems: 'center'
+    }
+
+})
 
 export default CartItem
