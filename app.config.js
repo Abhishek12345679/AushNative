9339094f436acd7fe5cb20d24ad09ce0ed768ec1
Abhishek12345679 <sah755146@gmail.com>
2022-03-03 13:39:05 +0530

    adding multiple app variants/flavours

---
+++ app.config.js
@@ -0,0 +1,10 @@
+export default {
+  name: 'Aushadhalay',
+  displayName: 'Aushadhalay',
+  android: {
+    package: 'com.aush.aush',
+  },
+  ios: {
+    bundleIdentifier: 'com.aush.aushj',
+  },
+};
